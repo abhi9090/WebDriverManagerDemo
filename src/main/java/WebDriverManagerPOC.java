7f7fc779fcaafeeadcad753df638c8bf45982d99
Abhilash Gadekar <abhilash.gadekar@gmail.com>
2021-11-09 21:53:42 +0530

    poc

---
+++ src/main/java/WebDriverManagerPOC.java
@@ -0,0 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.json.JsonOutput;
+
+public class WebDriverManagerPOC {
+    public static void main(String[] args) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.google.com");
+        System.out.println(System.getProperty("webdriver.chrome.driver"));
+        driver.quit();
+    }
+}
